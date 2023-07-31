b3e54820a728749913c7eeac222553105efd6e89
jdunhill <jtjdunhill@googlemail.com>
2023-07-31 14:57:07 +0100

    Changed id to type int instead of integer to avoid List issues

--- src/main/java/com/sky/spring/dog/rest/DogController.java
+++ src/main/java/com/sky/spring/dog/rest/DogController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,7 +45,7 @@ public class DogController {
     }
 
     @GetMapping("/get/{id}")
-    public Dog getById(@PathVariable Integer id) {
+    public Dog getById(@PathVariable int id) {
         System.out.println("ID: " + id);
         System.out.println(this.dogs.get(id));
         return this.dogs.get(id);
@@ -54,7 +53,7 @@ public class DogController {
 
    @PatchMapping("/update/{id}")
     public Dog update(
-            @PathVariable Integer id,
+            @PathVariable int id,
             @RequestParam("name") String name,
             @RequestParam("colour") String colour,
             @RequestParam("age") Integer age) {
@@ -70,7 +69,7 @@ public class DogController {
    }
 
    @DeleteMapping("/delete/{id}")
-    public boolean delete(@PathVariable Integer id) {
+    public Dog delete(@PathVariable int id) {
        System.out.println("DELETED. Dog at ID: " + id + " was deleted.");
        return this.dogs.remove(id);
    }
