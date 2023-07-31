e9bcd1ee50107b20e076b049d80a50bb74231a31
jdunhill <jtjdunhill@googlemail.com>
2023-07-31 10:01:52 +0100

    Init commit

---
+++ src/main/java/com/sky/spring/dog/SpringDogApplication.java
@@ -0,0 +1,13 @@
+package com.sky.spring.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/spring/dog/SpringDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.spring.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
