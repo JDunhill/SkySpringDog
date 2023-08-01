9f9264b39f231908fc53113e715e5d1f03b15f7c
jdunhill <jtjdunhill@googlemail.com>
2023-08-01 10:10:44 +0100

    Added h2 database

--- src/main/java/com/sky/spring/dog/domain/Dog.java
+++ src/main/java/com/sky/spring/dog/domain/Dog.java
@@ -1,12 +1,24 @@
 package com.sky.spring.dog.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Dog {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String name;
 
     private String colour;
 
     private Integer age;
+
 
 
     public Dog(String name, String colour, Integer age) {
